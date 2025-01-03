a3fc8f3330c5392ed23eeaab7ea94b4877e00d8d
kdxgautam <kdxgautam@gmail.com>
2025-01-04 01:36:53 +0530

    sanity

--- sanity/client.js
+++ sanity/client.js
@@ -4,6 +4,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2024-01-01",
+  withCredentials: true,
   useCdn: false,
 })
 
