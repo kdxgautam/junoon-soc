e9deee13c32fd1cd7631dc7a4a0b580d0294b8cd
kdxgautam <kdxgautam@gmail.com>
2025-01-03 23:46:52 +0530

    deply errors sanity cors

--- sanity/client.js
+++ sanity/client.js
@@ -4,6 +4,10 @@ export const client = createClient({
   projectId: "8mhyf2un",
   dataset: "production",
   apiVersion: "2024-01-01",
+  withCredentials: true,
   token: process.env.SANITY_API_TOKEN,
   useCdn: false,
 })
+
+
+
