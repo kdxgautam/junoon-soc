a63978beb74f78279949e8b9ca836ebe3bd8dc23
kdxgautam <kdxgautam@gmail.com>
2025-01-04 00:37:06 +0530

    sanity error env

--- sanity/client.js
+++ sanity/client.js
@@ -1,11 +1,11 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2024-01-01",
   withCredentials: true,
-  token: process.env.SANITY_API_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
   useCdn: false,
 })
 
